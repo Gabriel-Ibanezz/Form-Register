719128fbe8ad9a8d7ccfe82995b42605189906ec
Gabriel Ibañez <83628629+ShonenPako@users.noreply.github.com>
2021-05-05 16:48:27 -0300

    Add files via upload

---
+++ US.php
@@ -0,0 +1,35 @@
+
+<?php 
+
+
+$name = $_POST["nombre"];
+
+$lastname=$_POST["apellido"];
+
+$email=$_POST["correo"];
+
+$pass=$_POST["pass"];
+
+
+
+echo" Nombre: $name<br>"; 
+echo" Apellido: $lastname<br>";
+echo" Email: $email<br>";
+echo" Contraseña: $pass<br>";
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Datos</title>
+  <link rel="stylesheet" href="style4.css">
+</head>
+<body>
+  <p class="text" > !NO COMPARTAS TUS DATOS! POR USAR NUESTRA PAGINA WEB </p>
+  
+</body>
+</html>
---
+++ formulario.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="container-fluid">   
+                                <div class= "row"> 
+                                <div class="col-md-6">
+                                <div class="card-header">
+                                <a href="" class="btn btn-dark">Agregar nuevo Usuario</a>
+                                </div>
+
+                                    <table class="table table-striped table-dark mt-2"  > 
+                                                            <thead class="thead-dark" >
+
+                        <th ># </th>
+
+                        <th >Nombre</th>
+
+                        <th >Apellido</th>
+
+                        <th colspan ="2"   >  </th>
+          
+
+                                         </thead> 
+
+                      <tbody>
+                              <tr> 
+                                <th scope="row">       1         </td>
+
+                                             <td>    Gabriel     </td>
+
+                                             <td>    Ibanez       </td>
+                                             <td width="1   px"><a href="" class="btn btn-success btn-sm">Editar</a></td>
+                                             <td width="1px"><a href="" class="btn btn-danger btn-sm">Eliminar</a></td>   
+
+                    </tr> 
+                         <tr>
+                                <td scope="row">   2          </td>
+
+                                <td>  Alberto     </td>
+
+                                <td>  Ibañez       </td>
+                                <td width="1   px">
+                                    <a href="" class="btn btn-success btn-sm">Editar</a></td>
+                                 <td width="1px"><a href="" class="btn btn-danger btn-sm">Eliminar</a></td>  
+
+                    </tr>
+
+                
+
+                    <tr> 
+
+                                <th scope="row" >   3         </td>
+
+                                <td>  Profe                   </td>
+
+                                <td >  hackeado      </td>
+                                <td width="1   px"><a href="" class="btn btn-success btn-sm">Editar</a></td>
+                                             <td width="1px"><a href="" class="btn btn-danger btn-sm">Eliminar</a></td>  
+
+                                           
+
+
+
+                    </tr>
+
+                    
+
+             </tbody> 
+
+                                        </table>  
+
+                                </div>
+
+                                 </div>
+
+                                   </div>
+
+                                     </div>
+                                     
+                                  
+                                     
+                                     </div>   
+</body>
+</html>
+
+
+
+
+
+ 
